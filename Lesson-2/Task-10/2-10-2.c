6751f369b29d3f3eed67a4bb46425fb266c07a3d
Alyona Boriskina <99901626+ABoriskina@users.noreply.github.com>
2023-07-26 03:36:08 +0700

    Create 2-10-2.c

--- Lesson-2/Task-10/2-10-2.c
+++ Lesson-2/Task-10/2-10-2.c
@@ -4,20 +4,28 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main() {
-  int fd;
-  char buf[80];
-
-  char* fifoChannelName = "/tmp/fifo0001.1";
-  mkfifo(fifoChannelName, 0666);
-  
-  while (1) {
-      fd = open(fifoChannelName,O_WRONLY);
-      fgets(buf, 80, stdin);
-      write(fd, buf, strlen(buf)+1);
-      close(fd);
-      sleep(5);
-  }
-  exit(EXIT_SUCCESS);
+    int fd1;
+	  char buf[80];
+	
+    char* fifoChannelName = "/tmp/fifo0001.1";
+    mkfifo(fifoChannelName, 0666);
+    
+    int randomNumber;
+    srand(time(NULL));
+   
+    while (1) {
+        fd1 = open(fifoChannelName,O_WRONLY);
+        
+        randomNumber = rand() % 10000;
+        sprintf(buf, "%d", randomNumber);
+        
+        write(fd1, buf, strlen(buf)+1);
+        close(fd1);
+        sleep(5);
+    }
+    exit(EXIT_SUCCESS);
 }
